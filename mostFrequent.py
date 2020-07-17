f67daadfa41c74b60b868638e6dee5528e66a82f
Akshat Rana <65679227+Akshat511716@users.noreply.github.com>
2020-07-17 17:40:28 +0530

    Letter Frequency
    Frequency of a letter in a word in descending order of value

---
+++ mostFrequent.py
@@ -0,0 +1,12 @@
+my_string = input('Please Enter a string : ')
+freq = {}
+for i in my_string:
+    if i in freq:
+        freq[i] += 1
+    else:
+        freq[i] = 1
+f = dict(sorted(freq.items(), key = lambda x: x[1], reverse = True))
+for j, k in f.items():
+    print(f"{j} = {k}")    
+        
+
