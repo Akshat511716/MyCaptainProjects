caa0a0061600b4b6177426acc4fdd3f9fa36cd35
Akshat Rana <65679227+Akshat511716@users.noreply.github.com>
2020-07-24 22:57:06 +0530

    Update mostFrequent.py

--- mostFrequent.py
+++ mostFrequent.py
@@ -1,12 +1,13 @@
-my_string = input('Please Enter a string : ')
-freq = {}
-for i in my_string:
-    if i in freq:
-        freq[i] += 1
-    else:
-        freq[i] = 1
-f = dict(sorted(freq.items(), key = lambda x: x[1], reverse = True))
-for j, k in f.items():
-    print(f"{j} = {k}")    
+def mostFrequency:
+    my_string = input('Please Enter a string : ')
+    freq = {}
+    for i in my_string:
+        if i in freq:
+            freq[i] += 1
+        else:
+            freq[i] = 1
+    f = dict(sorted(freq.items(), key = lambda x: x[1], reverse = True))
+    for j, k in f.items():
+        print(f"{j} = {k}")    
         
 
